2baca67255adff2a3073920fac19a2974f6a9e7a
Chrismaganga <chrismaganga3@gmail.com>
2023-08-15 08:43:48 +0200

    add to main

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return ( 
+    <div className="p-4">
+  <Button className="text-blue-700">Dashboard page</Button>
+  </div>
+  )
+}
